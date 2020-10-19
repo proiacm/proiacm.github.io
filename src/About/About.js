4f35f3bd795cbc09ae31b01ebc78f6eb0588af10
proiacm <proiacm@gmail.com>
2020-10-19 12:03:28 -0600

    Update About.js

--- src/About/About.js
+++ src/About/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
             <div className='About'>  
                 <img className='About-img' src='https://i.imgur.com/oOhU8xs.jpg'></img>
                     <div className='About-text'>
-                        I leverage my empathy and experience in building client relationships
+                        I leverage empathy and experience in building client relationships
                         while designing functional, aesthetic applications. <br/> <br/>
                         As a full-stack developer experienced in React, JavaScript, and Ruby on Rails, I have worked on multiple projects on my own <br/>
                         and as part of a team, with a focus on object-oriented programming, MVC architecture, and responsive SPAs.<br/> <br/>
