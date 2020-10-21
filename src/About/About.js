8136f7bab25f972d9c47db96709cd47a4b3dfb21
proiacm <proiacm@gmail.com>
2020-10-21 09:50:16 -0600

    Update About and Project

--- src/About/About.js
+++ src/About/About.js
@@ -6,13 +6,13 @@ const About = (props) => {
             <div className='About'>  
                 <img className='About-img' src='https://i.imgur.com/oOhU8xs.jpg'></img>
                     <div className='About-text'>
-
                         I leverage empathy and experience in building client relationships
                         while designing functional, aesthetic applications. <br/> <br/>
+                        A versatile professional with 8+ years of work experience from start-ups to the largest companies in the USA. <br/><br/>
                         As a full-stack developer, I have worked on multiple projects on my own 
                         and as part of a team, <br/> with a focus on object-oriented programming, MVC architecture, and responsive SPAs.<br/> <br/>
                         <strong>Technical skills: React, JavaScript, Ruby on Rails, CSS, SQL </strong><br/> <br/>
-                        I'm always learning and am currently open to work or collaboration! <br/> <br/> 
+                        I'm always learning and am currently open to work opportunities or collaboration! <br/> <br/> 
                        <h3> Let's {props.loopText} </h3>
                     </div>
             </div>
