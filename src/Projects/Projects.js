2e5719d39944fd4a98da1827647d576ae7e76ea2
proiacm <proiacm@gmail.com>
2020-10-27 21:07:21 -0600

    CSS

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,7 +5,7 @@ const Projects = (props) => {
     
     return (
         <div className='Project-card'>
-            <h2 id='projects'><u>{props.name}</u></h2>
+            <h2><u>{props.name}</u></h2>
             <p>{props.desc}</p>
             <div className='Project'>
                 <a target='_blank' href={props.link}>
