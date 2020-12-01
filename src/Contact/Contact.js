6f29b45b900f7a3c8796b3dcb5f25a7c5a06df60
proiacm <proiacm@gmail.com>
2020-11-30 20:13:11 -0700

    Update Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,12 +6,11 @@ const Contact = (props) => {
     return (
         <div className='Contact-img'>
             <div className='Contact' id='contact'>
-                <h2 className='animated'>Let's {props.loopText}</h2><br/>                      
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/cmproia/'><img className='logo' src="https://i.imgur.com/PEDSkM6.png" alt="CiaraMaria's LinkedIn"/></a> &nbsp; &nbsp;
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://github.com/proiacm'><img className='logo' src="https://i.imgur.com/7Av2xFa.png" alt="CiaraMaria's GitHub Profile"/></a> &nbsp; &nbsp;
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'><img className='logo' src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="CiaraMaria's DEV Profile"/></a> &nbsp; &nbsp;
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href={"mailto:" + 'proiacm@gmail.com'}><img className='logo' src="https://i.imgur.com/GoTMYXF.png" alt="CiaraMaria's Email"/> </a>
-                <p className='location'><strong>I'm in Colorado Springs, CO -- also open to remote work</strong></p> <br/> <br/>
+                <h2 className='animated'>Let's {props.loopText}</h2> <br/>                 
+                Connect: <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/cmproia/'>LinkedIn</a> &nbsp; | &nbsp;
+                Code: <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://github.com/proiacm'>GitHub</a> &nbsp; | &nbsp;
+                Email: <a className='Contact-link' target='_blank' rel="noopener noreferrer" href={"mailto:" + 'proiacm@gmail.com'}>proiacm@gmail.com</a>
+                <p>© CiaraMaria Proia. All Rights Reserved.</p><br/> <br/>
                 <Link style={{ cursor: "pointer"}} to="nav" spy={true} smooth={true}><u><img src='https://i.imgur.com/hyJ4Bum.png' height="50" width="50"/></u></Link>
             </div>
         </div>
