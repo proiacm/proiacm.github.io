1f162e0d5102d6165b75ff81c84895b048e1d5f1
proiacm <proiacm@gmail.com>
2020-11-30 18:32:15 -0700

    Update Nav.js

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -10,7 +10,7 @@ const Nav = (props) => {
          <div className='Nav' id='nav'> 
                 <LottieAnimation lotti={home} height={300} width={300} />
                 {props.animatedText} <br/>
-                <h3 className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link></h3>
+                <h3 className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link> | <a target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></h3>
         </div>
     )
 }
