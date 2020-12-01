3765b1f0552754ed41436c53739f5b22c224e870
proiacm <proiacm@gmail.com>
2020-11-30 23:34:36 -0700

    Update Experience

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Experience.css';
-// import {Link} from 'react-scroll';
+import resume from '../Images/CiaraMaria_Proia_Resume.pdf';
 
 const Experience = () => {
 
@@ -9,11 +9,7 @@ const Experience = () => {
              <hr/>
             <h4 className='experience'>I am a versatile and driven professional with 8+ years of work experience ranging from start-ups 
                 to some of the largest companies in the USA.</h4>
-                <a href="/images/myw3schoolsimage.jpg" download>Download Resume</a>
-                {/* <img className='exp' src='https://i.imgur.com/AZ7wGW8.png'/> 
-                <img className='exp' src='https://i.imgur.com/WjyvtcA.png'/> 
-                <img className='exp' src='https://i.imgur.com/LG81wah.png'/> <br/> <br/> */}
-             {/* <Link style={{ cursor: "pointer"}} to="nav" spy={true} smooth={true}><u><img src='https://i.imgur.com/hyJ4Bum.png' height="50" width="50"/></u></Link> */}
+                <a className='myButton' href={resume} download>Download Resume</a>
         </div>
     )
 }
