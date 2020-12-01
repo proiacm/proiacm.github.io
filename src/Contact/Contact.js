1f02ea15a9af9ab99415b856adbb16f608124cec
proiacm <proiacm@gmail.com>
2020-12-01 14:29:56 -0700

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,8 +8,8 @@ const Contact = (props) => {
             <div className='Contact' id='contact'>
                 <h2 className='animated'>Let's {props.loopText}</h2> <br/>                 
                 <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/cmproia/'>LinkedIn</a> &nbsp; | &nbsp;
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://github.com/proiacm'>GitHub</a> &nbsp; | &nbsp;
-                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href={"mailto:" + 'proiacm@gmail.com'}>proiacm@gmail.com</a><br/><br/><br/><br/>
+                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href={"mailto:" + 'proiacm@gmail.com'}>proiacm@gmail.com</a> &nbsp; | &nbsp;
+                <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://github.com/proiacm'>GitHub</a><br/><br/><br/><br/>
                 <Link style={{ cursor: "pointer"}} to="nav" spy={true} smooth={true}><u><img src='https://i.imgur.com/hyJ4Bum.png' height="50" width="50"/></u></Link><br/> <br/>
                 <p className='footer'>© CiaraMaria Proia. All Rights Reserved.</p>
 
