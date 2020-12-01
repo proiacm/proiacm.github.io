3fc2ca07946c41f7c9d0d20868342565008054fc
proiacm <proiacm@gmail.com>
2020-12-01 09:34:41 -0700

    Update About

--- src/About/About.js
+++ src/About/About.js
@@ -5,7 +5,6 @@ import js from '../Images/js.png';
 import react from '../Images/react.png';
 import ruby from '../Images/ruby.png';
 import rails from '../Images/rails.png';
-import htmlcss from '../Images/htmlcss.png';
 import node from '../Images/node.png';
 import sqlite from '../Images/sqlite.png';
 import npm from '../Images/npm.png';
@@ -16,14 +15,14 @@ const About = () => {
             <div className='About'> 
                         <div className='About-text' id='about'>
                                 <img className='About-img' src={profile} alt='CiaraMaria'/>       
-                                <h1 className='fun'>Designer | Developer | Dog-person</h1>
-                                <h4>Leveraging experience in building client relationships and passion for <br/>solving problems
-                                while developing stunning, useable applications.</h4>
-                                <p>Fun fact: I have lived in North America, Central America, and Europe.</p>                                <img className='tech-stacktop' src={js} alt='js'/>
+                                <h1 className='About-title'>Developer | Designer | Dog-person</h1>
+                                <p>Leveraging experience in building client relationships and a passion for <br/>solving problems
+                                while developing stunning, useable applications.</p>
+                                <p className='funfact'>Fun fact: I have lived in North America, Central America, and Europe.</p>                                <img className='tech-stacktop' src={js} alt='js'/>
                                 <img className='tech-stack' src={react} alt='react'/>
                                 <img className='tech-stack' src={ruby} alt='ruby'/>  
                                 <img className='tech-stack' src={rails} alt='rails'/>  
-                                <img className='tech-stacktop' src={htmlcss} alt='htmlcss'/> <br/>
+                                 <br/>
                                 <img className='tech-stackbottom' src={node} alt='node'/>  
                                 <img className='tech-stackbottom' src={sqlite} alt='sqlite'/>  
                                 <img className='tech-stackbottom' src={npm} alt='npm'/>  
