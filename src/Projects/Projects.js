bd84e956b5b8e050298142d77ac9e73e190150f1
proiacm <proiacm@gmail.com>
2020-11-30 23:34:03 -0700

    Update Project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,10 +6,8 @@ const Projects = (props) => {
     return (
         <div className='Project-card' id='projects'>
             <h2><a target='_blank' rel="noopener noreferrer" href={props.link}>{props.name}</a></h2>
-            <p>{props.desc}</p>
-            <div className='Project'>
-                <iframe className='Project-img' src={props.video}/>
-            </div> 
+           <div className='Project-img'><img className='Project-img' src={props.demo}/></div> 
+            <p>{props.desc} &nbsp; &nbsp; &nbsp; {props.desc2}</p>
         </div>
     )
 }
