e6ea3c8d10143d5901250fbc8599be33e799a9b5
proiacm <proiacm@gmail.com>
2020-12-01 01:20:25 -0700

    Update Project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,10 +4,10 @@ import './Projects.css';
 const Projects = (props) => {
     
     return (
-        <div className='Project-card' id='projects'>
-            <h2><a target='_blank' rel="noopener noreferrer" href={props.link}>{props.name}</a></h2>
-           <div className='Project-img'><img className='Project-img' src={props.demo}/></div> 
-            <p>{props.desc} &nbsp; &nbsp; &nbsp; {props.desc2}</p>
+        <div className='Project-card'>
+            <h3><a target='_blank' rel="noopener noreferrer" href={props.link}>{props.name}</a></h3>
+           <img className='image' src={props.demo}/>
+            <p className='text'>{props.desc}</p>
         </div>
     )
 }
