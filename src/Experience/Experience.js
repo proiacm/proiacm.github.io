e104dcb83eabe6a335fea777dd3713d17b459d65
proiacm <proiacm@gmail.com>
2020-12-04 11:00:36 -0700

    Update Experience

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -7,8 +7,8 @@ const Experience = () => {
     return (
         <div className='Experience'>
              <hr/>
-            <p className='experience'>I am a versatile and driven professional with 8+ years of work experience ranging from start-ups 
-                to some of the largest companies in the USA.</p>
+            <p className='exp-text'>I am a versatile and driven professional with 8+ years of work experience from start-ups 
+                to the largest companies in the USA. Let's discuss opportunities!</p>
                 <a className='myButton' href={resume} download>Download Resume</a>
         </div>
     )
