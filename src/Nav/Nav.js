02ae5c1d42f2cbd8c01838203078ad3491950a35
proiacm <proiacm@gmail.com>
2020-12-04 11:00:46 -0700

    Update Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './Nav.css';
 import {Link} from 'react-scroll'
-import LottieAnimation from '../Lottie';
-import home from '../Animation/dev.json';
 
 const Nav = (props) => {
 
     return ( 
-         <div className='Nav' id='nav'> 
-                <LottieAnimation lotti={home} height={300} width={300} />
-                <h3 className='Animated'>{props.animatedText} </h3>
-                <p className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link> | <a target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
+         <div className='home-title' id='nav'> 
+          <span>Hi, I'm CiaraMaria!</span>
+                <p className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link> | <a className='blog' target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
         </div>
     )
 }
