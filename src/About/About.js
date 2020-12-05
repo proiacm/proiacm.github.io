3c31befc4f5e00f5219b7ec3acf251ac3211a799
proiacm <proiacm@gmail.com>
2020-12-04 20:47:38 -0700

    Merge Experience component with About component

--- src/About/About.js
+++ src/About/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './About.css';
-import profile from '../Images/ciaramaria.webp';
+import profile from '../Images/cm.png';
+import resume from '../Images/CiaraMaria_Proia_Resume.pdf';
 
 const About = () => {
     return (
-            <div className='About'  id='about'> 
-                        <div className='container'>
+            <div className='About'> 
+                        <div className='container' id='about'>
                                 <img className='About-img' src={profile} alt='CiaraMaria'/> 
                                 <div class="overlay">
                                         <div class="overlay-text">
@@ -13,11 +14,15 @@ const About = () => {
                                         </div>
                                 </div>
                         </div>
-                                <div className='About-text'>
+                        <div className='About-text'>
                                 <h2 className='About-title'>Web & App Developer | Designer </h2>
                                 <p>Leveraging experience in building client relationships, a passion for solving problems, and an eye for aesthetics
-                                while developing stunning useable applications. My current tech stack inlcudes: <strong>JavaScript, npm, React, Ruby on Rails, Firebase,
-                                Node, and SQlite</strong> </p>
+                                while developing stunning and intuitive applications. <br/>I love being a perpetual learner and am continuously expanding my knowledge
+                                on current and new technology through Udemy as well as technical writing. My current tech stack inlcudes: <br/><strong>JavaScript, npm, React, Ruby on Rails, Firebase,
+                                Node, and SQlite</strong><hr/>
+                                I am a versatile and driven professional with 8+ years of work experience from start-ups 
+                                to the largest companies in the USA. Let's discuss opportunities!</p>
+                                <a className='myButton' href={resume} download>Download Resume</a>
                         </div>
             </div>
     )
