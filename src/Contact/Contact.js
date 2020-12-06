774630be58b3532c4b6a02baae93f413c7d921d7
proiacm <proiacm@gmail.com>
2020-12-06 00:03:04 -0700

    Update Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,6 +3,7 @@ import './Contact.css';
 import {Link} from 'react-scroll';
 
 const Contact = (props) => {
+
     return (
         <div className='Contact-img'>
             <div className='Contact' id='contact'>
@@ -10,7 +11,7 @@ const Contact = (props) => {
                 <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/cmproia/'>LinkedIn</a> &nbsp; | &nbsp;
                 <a className='Contact-link' target='_blank' rel="noopener noreferrer" href={"mailto:" + 'proiacm@gmail.com'}>proiacm@gmail.com</a> &nbsp; | &nbsp;
                 <a className='Contact-link' target='_blank' rel="noopener noreferrer" href='https://github.com/proiacm'>GitHub</a><br/><br/><br/><br/>
-                <Link style={{ cursor: "pointer"}} to="nav" spy={true} smooth={true}><u><img src='https://i.imgur.com/hyJ4Bum.png' height="50" width="50"/></u></Link><br/> <br/>
+                <Link style={{ cursor: "pointer"}} to="nav" spy={true} smooth={true}><u>Back to top</u></Link><br/>
                 <p className='footer'>© CiaraMaria Proia. All Rights Reserved.</p>
 
             </div>
