137567f1f7c4837ab0c957eaba62f27a8cd680e8
proiacm <proiacm@gmail.com>
2020-12-06 00:02:57 -0700

    Update Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,9 +5,9 @@ import {Link} from 'react-scroll'
 const Nav = (props) => {
 
     return ( 
-         <div className='home-title' id='nav'> 
-          <span>Hi, I'm CiaraMaria!</span>
-                <p className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link> | <a className='blog' target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
+         <div className='home-title'> 
+          <span>CiaraMaria</span>
+                <p className='Nav-bar'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link> | <Link style={{ cursor: "pointer"}} to="resume" spy={true} smooth={true}><u>Resume</u></Link> | <Link style={{ cursor: "pointer" }} to="projects" spy={true} smooth={true}><u>Projects</u></Link> | <Link style={{ cursor: "pointer" }} to="contact" spy={true} smooth={true}><u>Contact</u></Link> | <a className='blog' target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
         </div>
     )
 }
