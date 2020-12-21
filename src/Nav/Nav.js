5ad5c5c7d643b60782dff471049337a96f4412f0
proiacm <proiacm@gmail.com>
2020-12-21 00:35:54 -0700

    Fix nav link order

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -9,8 +9,9 @@ const Nav = () => {
          <div className='nav' id='home'> 
                 <p className='nav-bar-left'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}>About</Link></p>
                 <p className='nav-bar-left'><Link style={{ cursor: "pointer"}} to="work" spy={true} smooth={true}>Work</Link></p>
-                <p className='nav-bar-right'><Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link></p>
                 <p className='nav-bar-right'><a target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
+                <p className='nav-bar-right'><Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link></p>
+                
                 <img className='nav-logo' src={logo} alt='logo'/>
         </div>
     )
