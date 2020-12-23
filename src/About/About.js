6d2c34509e8d33e241d8397f863a1a14320d5f49
proiacm <proiacm@gmail.com>
2020-12-23 15:12:03 -0700

    Revise text

--- src/About/About.js
+++ src/About/About.js
@@ -9,10 +9,10 @@ const About = () => {
                 <img className='about-img' src={portrait} alt='CiaraMaria'/> 
                         <h1 className='about-title'>Developer & Designer </h1> 
                         <p className='about-text'>Leveraging experience in building client relationships while designing stunning and intuitive applications. Developer with an eye for aesthetics and an attitude for solving problems.
-                        I love being a perpetual learner, constantly growing from experience and failure; and challenging my understanding of concepts through project building, refactoring old code, Udemy courses, and technical blogging.
+                        I love being a perpetual learner, constantly growing from experience and failure; and challenging my understanding of concepts through project building, refactoring old code, Udemy courses, and technical blogging. 
                         I am a versatile and driven professional with 8+ years of work experience from start-ups 
                         to some of the largest companies in the USA. <br/> <br/> 
-                        Current tech stack: <strong>JavaScript, React, Ruby on Rails, Firebase, Node, and SQlite</strong><br/> <br/>
+                        Current tech stack: <strong>React, CSS, JavaScript, Ruby on Rails, Firebase, Node, and SQlite</strong><br/> <br/>
                         <strong>Fun fact:</strong> I have lived in North America, Central America, and Europe. <br/>
                         <strong>Hobbies:</strong> reading, travel, weightlifting, camping, gaming, and coffee (if that counts).
                         </p>
