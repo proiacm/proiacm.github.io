e92e064302076718bca0767df720c109ea241cbd
proiacm <proiacm@gmail.com>
2020-12-22 23:09:25 -0700

    Style for mobile

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Nav.css';
 import {Link} from 'react-scroll';
+import logo from '../Images/cmlogotrans.png';
+
 
 const Nav = () => {
 
@@ -10,8 +12,9 @@ const Nav = () => {
                 <p className='nav-bar-left'><Link style={{ cursor: "pointer"}} to="work" spy={true} smooth={true}>Projects</Link></p>
                 <p className='nav-bar-right'><a target='_blank' rel="noopener noreferrer" href='https://dev.to/proiacm'>Blog</a></p>
                 <p className='nav-bar-right'><Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link></p>
-
+                <img className='nav-logo' src={logo} alt='logo'/>
                 <p className='logo'>Ciara <br/> Maria</p>
+                <p className='logo2'>Web & Application Developer</p>
                 <svg className='svg' width="800" height="600" xmlns="http://www.w3.org/2000/svg">
                 <rect className='path' stroke="#000" id="svg_3" height="318.99999" width="298.99999" y="125.45313" x="157.5" fill-opacity="null" stroke-opacity="null" stroke-width="5" fill="none"/>
                 <rect className='path2' stroke="#000" id="svg_4" height="316.99999" width="304" y="103.45313" x="132.5" fill-opacity="null" stroke-opacity="null" stroke-width="5" fill="none"/>
